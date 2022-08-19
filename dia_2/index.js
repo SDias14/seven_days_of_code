d453d957373cfab74dcd313fbd580141cfa8fc91
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-19 10:14:03 -0300

    dia 02

---
+++ dia_2/index.js
@@ -0,0 +1,16 @@
+let name = prompt('Qual o seu nome')
+let age = prompt('Quantos anos voce têm?')
+let progLanguage = prompt('Qual linguagem de programação você está estudando?')
+
+
+alert(`Olá ${name}  você tem ${age} anos e já está aprendendo ${progLanguage}`)
+
+
+let answer = prompt('Você gosta de programar?')
+
+if (answer == 1) {
+    alert('Muito bom! Continue estudando e você terá muito sucesso.')
+} else {
+    alert('Ahh que pena... Já tentou aprender outras linguagens?')
+} 
+
